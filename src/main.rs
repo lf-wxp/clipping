b19a721734882d50b4ddc941f3834d259acc1209
franciscowxp <franciscowxp@gmail.com>
2023-03-05 16:07:43 +0800

    feat: finish the text parse processor

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ fn main() -> Result<()> {
   let cli = args::Cli::parse();
   match cli.command {
     args::Commands::Parse(args) => {
-      parse::parse_book("乌合之众:大众心理研究 (社会学经典名著) (古斯塔夫·勒宠)");
     },
   };
   Ok(())
--- src/parse.rs
+++ src/parse.rs
@@ -1,8 +1,14 @@
+use chrono::prelude::*;
 use nom::{
   self,
-  bytes::complete::{tag, take_until, take_until1},
-  sequence::delimited,
+  bytes::complete::{tag, take_till, take_until, take_until1},
+  sequence::{delimited, preceded, tuple},
   IResult,
+};
+use nom_unicode::{
+  complete::{alpha0, digit0, space0},
+  is_numeric,
+  is_alphabetic
 };
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, Lines};
@@ -14,6 +20,7 @@ pub type UResult<T> = std::result::Result<T, UError>;
 pub struct Book {
   title: String,
   author: String,
+  clipping: Vec<Clipping>,
 }
 
 impl Book {
@@ -21,22 +28,36 @@ impl Book {
     Book {
       title: title.trim().to_owned(),
       author: author.trim().to_owned(),
+      clipping: vec![],
     }
+  }
+  fn add_clipping(&mut self, clipping: Clipping) {
+    self.clipping.push(clipping);
   }
 }
 
-#[derive(Debug)]
-struct DateTime {
-  time: String,
-  date: String,
-}
-#[derive(Debug)]
-struct Clipping {
-  book: Book,
-  datetime: DateTime,
+#[derive(Debug, PartialEq)]
+pub struct Clipping {
+  date_time: DateTime<Utc>,
   position: String,
   clipping: String,
   mark: Option<String>,
+}
+
+impl Clipping {
+  fn new(
+    clipping: String,
+    position: String,
+    date_time: DateTime<Utc>,
+    mark: Option<String>,
+  ) -> Clipping {
+    Clipping {
+      date_time,
+      position,
+      clipping,
+      mark,
+    }
+  }
 }
 
 fn read_lines<P>(path: P) -> io::Result<Lines<BufReader<File>>>
@@ -47,18 +68,46 @@ where
   Ok(BufReader::new(file).lines())
 }
 
-pub fn extrack_bracket_content(line: &str) -> IResult<&str, &str> {
+pub fn extract_bracket_content(line: &str) -> IResult<&str, &str> {
   let (line, content) = delimited(tag("("), take_until(")"), tag(")"))(line.trim())?;
   if !line.is_empty() {
-    return extrack_bracket_content(line);
+    return extract_bracket_content(line);
   }
   Ok((line, content))
 }
 
 pub fn parse_book(line: &str) -> Result<Book, nom::Err<nom::error::Error<&str>>> {
   let (line, title) = take_until1("(")(line.trim())?;
-  let (_, author) = extrack_bracket_content(line)?;
+  let (_, author) = extract_bracket_content(line)?;
   Ok(Book::new(title, author))
+}
+
+pub fn get_number(line: &str) -> Result<(&str, u32), nom::Err<nom::error::Error<&str>>> {
+  let (remain, number) = preceded(take_till(is_numeric), digit0)(line)?;
+  Ok((remain, number.to_owned().parse::<u32>().unwrap()))
+}
+
+pub fn parse_date_time(line: &str) -> Result<DateTime<Utc>, nom::Err<nom::error::Error<&str>>> {
+  let (line, year) = get_number(line)?;
+  let (line, month) = get_number(line)?;
+  let (line, day) = get_number(line)?;
+  let (line, (_, _, am_of_pm)) = tuple((alpha0, space0, alpha0))(line)?;
+  let (line, hour) = get_number(line)?;
+  let (line, minute) = get_number(line)?;
+  let (_, second) = get_number(line)?;
+  let time_offset = if am_of_pm == "上午" { 0 } else { 12 };
+  Ok(
+    Utc
+      .with_ymd_and_hms(year as i32, month, day, hour + time_offset, minute, second)
+      .unwrap(),
+  )
+}
+
+pub fn parse_clipping(lines: Vec<&str>) -> Result<Clipping, nom::Err<nom::error::Error<&str>>> {
+  let (remain, position) = preceded(take_till(is_numeric), take_till(is_alphabetic))(lines[0])?;
+  let date_time = parse_date_time(remain)?;
+  Ok(Clipping::new(lines[1].to_owned(), position.to_owned(), date_time, None))
+
 }
 
 pub fn parse(path: PathBuf) -> Result<(), io::Error> {
@@ -88,9 +137,24 @@ mod tests {
   pub fn test_parse_book() -> UResult<()> {
     let parsed_book = parse_book("乌合之众:大众心理研究 (社会学经典名著) (古斯塔夫·勒宠)")?;
     let book = Book::new("乌合之众:大众心理研究", "古斯塔夫·勒宠");
-
     assert_eq!(book, parsed_book);
     Ok(())
   }
 
+  #[test]
+  pub fn test_parse_date_time() -> UResult<()> {
+    let parsed_date_time = parse_date_time("添加于 2015年2月14日星期六 下午3:21:03")?;
+    let date_time = Utc.with_ymd_and_hms(2015, 2, 14, 15, 21, 3).unwrap();
+    assert_eq!(date_time, parsed_date_time);
+    Ok(())
+  }
+
+  #[test]
+  pub fn test_parse_clipping() -> UResult<()> {
+    let lines = vec!["- 您在位置 #116-119的标注 | 添加于 2015年2月14日星期六 下午3:21:03", "在中国，任何超脱飞扬的思想都会砰然坠地的，现实的引力太沉重了。"];
+    let clipping = Clipping::new(lines[1].to_owned(), "116-119".to_owned(), Utc.with_ymd_and_hms(2015, 2, 14, 15, 21, 3).unwrap(), None);
+    let parsed_clipping = parse_clipping(lines)?;
+    assert_eq!(clipping, parsed_clipping);
+    Ok(())
+  }
 }
